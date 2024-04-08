5ea903088b5a5c7a32546e12526399a237b97e87
hunter87ff <huntergaming702@gmail.com>
2024-04-08 20:06:36 +0530

    updated overall functionality

--- src/extension.ts
+++ src/extension.ts
@@ -3,71 +3,67 @@ import { basename, dirname, extname } from "path";
 import * as vscode from "vscode";
 
 // let extensionUri: vscode.Uri;
+let stopDisposable: vscode.Disposable;
+let runDisposable: vscode.Disposable;
+let restartDisposable: vscode.Disposable;
 
-
-function isCommandAvailable(command: string): boolean {
-    try {
-        const commandToRun = process.platform === 'win32' ? 'where' : 'which';
-        execSync(`${commandToRun} ${command}`);
-        return true;
-    } catch (error) {
-        return false;
-    }
-}
 
 export function activate(context: vscode.ExtensionContext) {
-    const config = vscode.workspace.getConfiguration('dry-runner');
+    const config = vscode.workspace.getConfiguration('dry-runner'); //package.json
+    const compilerPath = config.get('compilerPath') as string;
+    let isEnv = process.env.path?.toLowerCase().includes("mingw");
+    const outputChannel = vscode.window.createOutputChannel("Dry Runner");
+    const outlog = (message: string) => outputChannel.appendLine(message);
+    const errlog = (message: string) => vscode.window.showErrorMessage(message);
     const isWin = process.platform === 'win32';
     let terminal: vscode.Terminal | undefined;
-    const outputChannel = vscode.window.createOutputChannel("Dry Runner");
-    const outlog = (message: string) => outputChannel.appendLine(`[INFO] ${message}`);
-    if(!isCommandAvailable('gcc') || !isCommandAvailable('g++')){
-        vscode.window.showErrorMessage("You can update compiler path in settings.");}
-        const compilerPath = config.get('compilerPath') as string;
-        if (!process.env.path?.includes(compilerPath)) {
-            process.env.path += compilerPath + ';';
-        }
-        
+    let compiler:string;
+    let c_compiler:string;
+    let cpp_compiler:string;
+    if(isWin && isEnv==false){
+        c_compiler = `${compilerPath}\\gcc.exe`;
+        cpp_compiler = `${compilerPath}\\g++.exe`;
+    }else{
+        c_compiler = 'gcc';
+        cpp_compiler = 'g++';
+    }
 
   const run = async () => {
     outputChannel.clear();
-    let document;
+    let document: vscode.TextDocument | undefined = undefined;
     for (let textEditor of vscode.window.visibleTextEditors) {
       const fileName = textEditor.document?.fileName || "";
-      if (extname(fileName) === '.c' || extname(fileName) === '.cpp') {
+      if (extname(fileName) === '.c' || extname(fileName) === '.cpp'){
         document = textEditor.document;
-      }
-    }
-    const fileName = document?.fileName || "";
-    vscode.commands.executeCommand("setContext", "dry-runner.running", true);
-    await document?.save();
-    const fileBaseName = basename(fileName);
-    const fileBaseNameWithoutExt = basename(fileName, extname(fileName)).replace(" ", "_");
-    const fileDirectory = dirname(fileName);
-    let compiler;
-    terminal = vscode.window.createTerminal({name: 'Dry Runner',});
-    if (document?.languageId === 'c') {
-        if(isWin){compiler = `gcc.exe`;}
-        else{compiler = 'gcc';}
-        terminal.sendText(`gcc "${fileBaseName}" -o "${fileBaseNameWithoutExt}.exe"; ./${fileBaseNameWithoutExt}.exe`);
-    }
-    else {
-        if(isWin){compiler = `g++.exe`;}
-        else{compiler = 'g++';}
-        terminal.sendText(`g++ "${fileBaseName}" -o "${fileBaseNameWithoutExt}.exe"; ./${fileBaseNameWithoutExt}.exe`);
         }
-    terminal.show();
-    // vscode.commands.executeCommand("setContext", "dry-runner.running", false);
-  };
+    }
+    if (document!==undefined) {
+        const fileName = document?.fileName || "";
+        vscode.commands.executeCommand("setContext", "dry-runner.running", true);
+        await document?.save();
+        const fileBaseName = basename(fileName);
+        const fileBaseNameWithoutExt = basename(fileName, extname(fileName)).replace(" ", "_");
+        const fileDirectory = dirname(fileName);
+        terminal = vscode.window.createTerminal({name: 'Dry Runner',});
+        if(document?.languageId==='c'){compiler = c_compiler;}
+        else if (document?.languageId==='cpp'){compiler = cpp_compiler;}
+        if(isWin){
+            terminal.sendText(`${compiler}  "${fileDirectory}\\${fileBaseName}" -o "${fileDirectory}\\${fileBaseNameWithoutExt}.exe"; ${fileDirectory}\\${fileBaseNameWithoutExt}.exe`);
+        }
+        else{
+            terminal.sendText(`${compiler}  "${fileBaseName}" -o "${fileBaseNameWithoutExt}.exe"; ./${fileBaseNameWithoutExt}.exe`);
+        }
+        terminal.show();
+    }
+};
 
-  const runDisposable = vscode.commands.registerCommand("dry-runner.run", run);
-  const stopDisposable = vscode.commands.registerCommand("dry-runner.stop", () => {
+  runDisposable = vscode.commands.registerCommand("dry-runner.run", run);
+  stopDisposable = vscode.commands.registerCommand("dry-runner.stop", () => {
     vscode.commands.executeCommand("setContext", "dry-runner.running", false);
     terminal?.dispose();});
 
-  const restartDisposable = vscode.commands.registerCommand("dry-runner.restart", async () => {
-    terminal?.dispose();
-    run();
+  restartDisposable = vscode.commands.registerCommand("dry-runner.restart", async () => {
+    terminal?.dispose();run();
   });
 
   context.subscriptions.push(runDisposable);
@@ -75,5 +71,14 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(restartDisposable);
 }
 
+vscode.window.onDidCloseTerminal(t => {
+    if (t.exitStatus && t.exitStatus.code) {
+        vscode.window.showInformationMessage(`Exit code: ${t.exitStatus.code}`);
+    }
+  });
 
-export function deactivate() { }
+export function deactivate() {
+    stopDisposable.dispose();
+    runDisposable.dispose();
+    restartDisposable.dispose();
+}
